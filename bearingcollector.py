e74f5067cac97b61201e7148e33bdf220f1c5e89
Mourcat <vybornovs@rambler.ru>
2022-07-06 05:31:42 +0300

    BearingCollector app created from template.

---
+++ bearingcollector.py
@@ -0,0 +1,29 @@
+import os
+import platform
+
+from kivy.core.window import Window
+from kivymd.app import MDApp
+
+from libs.uix.baseclass.root import Root
+
+# This is needed for supporting Windows 10 with OpenGL < v2.0
+if platform.system() == "Windows":
+    os.environ["KIVY_GL_BACKEND"] = "angle_sdl2"
+
+
+class BearingCollector(MDApp): 
+    def __init__(self, **kwargs):
+        super(BearingCollector, self).__init__(**kwargs)
+        Window.soft_input_mode = "below_target"
+        self.title = "Bearing Collector"
+
+        self.theme_cls.primary_palette = "Blue"
+        self.theme_cls.primary_hue = "300"
+
+        self.theme_cls.accent_palette = "Red"
+        self.theme_cls.accent_hue = "300"
+
+        self.theme_cls.theme_style = "Dark"
+
+    def build(self):
+        return Root()
---
+++ main.py
@@ -0,0 +1,40 @@
+import os
+import sys
+
+root_dir = os.path.split(os.path.abspath(sys.argv[0]))[0]
+sys.path.insert(0, os.path.join(root_dir, "libs", "applibs"))
+
+import json
+import traceback 
+
+from kivy.factory import Factory 
+from bearingcollector import BearingCollector
+
+__version__ = "1.0"
+
+"""
+Registering factories from factory.json.
+"""
+r = Factory.register
+
+with open("factory_registers.json") as fd:
+    custom_widgets = json.load(fd)
+    for module, _classes in custom_widgets.items():
+        for _class in _classes:
+            r(_class, module=module)
+
+
+try:
+    BearingCollector().run()
+except Exception:
+    error = traceback.format_exc()
+
+    """
+    If the app encounters an error it automatically saves the
+    error in a file called ERROR.log.
+    You can use this for BugReport purposes.
+    """
+    with open("ERROR.log", "w") as error_file:
+        error_file.write(error)
+
+    print(error)
